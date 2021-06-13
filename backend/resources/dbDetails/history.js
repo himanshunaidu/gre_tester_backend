7c9706dfe921d8cb88ebeaab8d7046e0822da28c
Himanshu First <himanshunaidu12161997@gmail.com>
2021-06-13 22:04:14 +0530

    DB Schema config

---
+++ backend/resources/dbDetails/history.js
@@ -0,0 +1,30 @@
+//Table Name
+const HISTORY="history";
+
+//history columns
+const HISTORY_ID="history_id";
+const NAME="name";
+const RANDOM="random";
+const INDEX_START="index_start";
+const MEANING="meaning";
+const MEANING_SC="meaning_sc";
+const MNEMONIC="mnemonic";
+const MNEMONIC_SC="mnemonic_sc";
+const SENTENCE="sentence";
+const SENTENCE_SC="sentence_sc";
+const TOTAL="total";
+
+module.exports = {
+    TABLE_NAME: HISTORY,
+    HISTORY_ID: HISTORY_ID,
+    NAME: NAME,
+    RANDOM: RANDOM,
+    INDEX_START: INDEX_START,
+    MEANING: MEANING,
+    MEANING_SC: MEANING_SC,
+    MNEMONIC: MNEMONIC,
+    MNEMONIC_SC: MNEMONIC_SC,
+    SENTENCE: SENTENCE,
+    SENTENCE_SC: SENTENCE_SC,
+    TOTAL: TOTAL
+}
---
+++ backend/resources/dbDetails/word_meanings.js
@@ -0,0 +1,28 @@
+//Table Name
+const WORD_MEANINGS="word_meanings";
+
+//word_meanings columns
+const WID="wid";
+const WORD="word";
+const MNEMONIC="mnemonic";
+const EXAMPLE="example";
+const VERB="Verb";
+const ADJECTIVE="Adjective";
+const NOUN="Noun";
+const ADVERB="Adverb";
+const OTHERS="Others";
+const MAIN_ID="main_id";
+
+module.exports = {
+    TABLE_NAME: WORD_MEANINGS,
+    WID: WID,
+    WORD: WORD,
+    MNEMONIC: MNEMONIC,
+    EXAMPLE: EXAMPLE,
+    VERB: VERB,
+    ADJECTIVE: ADJECTIVE,
+    NOUN: NOUN,
+    ADVERB: ADVERB,
+    OTHERS: OTHERS,
+    MAIN_ID: MAIN_ID
+}
