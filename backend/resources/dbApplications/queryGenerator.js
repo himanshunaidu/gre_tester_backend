cebe88602acdabed59bd4e8ec0a44b21ebeb4f0c
Himanshu First <himanshunaidu12161997@gmail.com>
2021-06-13 22:03:03 +0530

    Sample query generator (not in use)

---
+++ backend/resources/dbApplications/queryGenerator.js
@@ -0,0 +1,9 @@
+//Generate a select query that takes table_name, columns, where, orderby, groupby, having
+const selectQuery = () => {
+
+}
+
+//Generate an insert query that takes table_name, columns, values
+const insertQuery = () => {
+    
+}
